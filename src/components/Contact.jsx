4691b93af674e1b670037c4465b4516419e7f79d
Minh Vy ha <minhvy.ha@outlook.com>
2023-05-16 21:35:12 +1000

    update emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,7 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -30,19 +31,20 @@ const Contact = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoading(true);
-
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        import.meta.env.VITE_EMAILJS_SERVICE_ID
+,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID
+,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Minh Vy Ha",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "minhvy.ha@outlook.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
