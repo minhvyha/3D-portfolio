066c64f4bb77c0241dcd1ffae288835d004c4de6
Minh Vy ha <minhvy.ha@outlook.com>
2023-05-26 22:00:11 +1000

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -73,7 +73,7 @@ const Navbar = () => {
           <img
             src={toggle ? close : menu}
             alt="menu"
-            className="w-[28px] h-[28px] object-contain"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
             onClick={() => setToggle(!toggle)}
           />
 
