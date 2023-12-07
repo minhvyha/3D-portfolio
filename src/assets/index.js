ed48011ea68c960f0e2cc6b57fed298d2cf37153
Minh Vy ha <minhvy.ha@outlook.com>
2023-12-07 23:40:22 +1100

    update

--- src/assets/index.js
+++ src/assets/index.js
@@ -49,7 +49,7 @@ import electron from './tech/electron.png';
 import greenieweb from './company/greenieweb.png';
 import chs from './company/chs.png';
 import projectflux from './company/projectflux.png';
-import gads from './company/gads.png'
+import gads from './company/gads.png';
 
 export {
   beginner,
@@ -62,7 +62,8 @@ export {
   certificateImg,
   search,
   businesscard,
-  firewithin,gads,
+  firewithin,
+  gads,
   mern1,
   mern2,
   path,
